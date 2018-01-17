3e40bab0dd3e3ccaa30be29bc45985d7c514e0a5
Draivun <nick_goris@outlook.com>
2018-01-17 11:54:08 +0100

    Fixed CMake issue under Windows

--- project/source/main.cpp
+++ project/source/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 #include "game.hpp"
 
 int main(void) {
-    auto game = Game();
+   // auto game = Game();
+
+	sf::RenderWindow window(sf::VideoMode(512, 512), "EAmpire Tower Defense", sf::Style::Default);
+
+	while( window.isOpen()) {
+		sf::Event evnt;
+		while (window.pollEvent(evnt)) {
+
+			switch(evnt.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::Resized:
+				std::cout << "Window Resized" << std::endl;
+				break;
+			}
+		}
+		window.clear();
+		window.display();
+	}
 
     return 0;
 }
