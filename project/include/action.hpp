005387d42c2d298a2a2dfc0fd7847f0a291f6143
Dylan van Eck <dylan.v.eck@hotmail.com>
2018-01-17 16:09:58 +0100

    add action class for testing the enemy_a methods

---
+++ project/include/action.hpp
@@ -0,0 +1,18 @@
+#ifndef _ACTION_HPP
+#define _ACTION_HPP
+#include <SFML\Graphics.hpp>
+#include <functional>
+class action {
+private:
+	std::function< bool() > condition;
+	std::function< void() > work;
+
+public:
+	action(std::function<bool()> condition, std::function< void()> work);
+	action(sf::Keyboard::Key key, std::function< void()> work);
+	action(sf::Mouse::Button button, std::function<void()> work);
+	action(std::function< void() > work);
+	
+	void operator()();
+};
+#endif // !_ACTION_HPP
