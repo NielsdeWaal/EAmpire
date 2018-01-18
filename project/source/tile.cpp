5c4ae0ea1c5fa0a0f9cbf4a74181bc10116ec452
Jasper <Jasper.Smienk@student.hu.nl>
2018-01-18 13:00:17 +0100

    Add files via upload

---
+++ project/source/tile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "tile.hpp"
+
+Tile::Tile(bool is_navigable):
+	navigable(is_navigable)
+	{}
+
+void Tile::set_navigability(bool navigability) {
+	navigable = navigability;
+}
+
+bool Tile::is_navigable() {
+	return navigable;
+}
