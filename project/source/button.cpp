4e85f32abeef6287759f122d637d91d619191b1c
Draivun <nick_goris@outlook.com>
2018-01-19 11:51:49 +0100

    Updated comments to fit predetermined style

--- project/source/button.cpp
+++ project/source/button.cpp
@@ -1,10 +1,5 @@
 #include "button.hpp"
 
-/// \brief		Button constructor
-/// \details	This constructor is called when creating a clickable button screen object. Window needs to be passed as an argument, because 
-///				it is used in the function that checks whether or not the mouse is over the button.
-/// \param		std::string s : std::string s - Contains the text displayed on the button
-///	
 Button::Button(std::string& s, sf::Vector2f location, sf::Vector2f size, sf::RenderWindow& window):
 	s(s),
 	location(location),
@@ -14,9 +9,6 @@ Button::Button(std::string& s, sf::Vector2f location, sf::Vector2f size, sf::Ren
 	setup();
 }
 
-/// \brief Button setup function
-/// \details This function initializes the button. It is called in the constructor.
-///
 void Button::setup() {
 	if (!font.loadFromFile("PlayfairDisplay-Black.ttf")) {
 		//handle error
@@ -33,33 +25,19 @@ void Button::setup() {
 	boundary.setFillColor(sf::Color::Green);
 }
 
-/// \brief		Draw function
-/// \details	This function draws the button objects to the buffer, window.display() then displays them
-///
 void Button::draw() {
 	window.draw(boundary);
 	window.draw(text);
 }
 
-/// \brief		Get location function for button
-/// \ details	This function returns the coordinates of the top left corner of the button
-/// \return		The top left corner coordinate of the button in the form of a Vector2F
 sf::Vector2f Button::get_location() {
 	return location;
 }
 
-/// \brief		Get size function for button
-/// \ details	This function returns the coordinates of the bottom right corner of the button
-/// \return		The bottom right corner coordinate of the button in the form of a Vector2F
 sf::Vector2f Button::get_size() {
 	return size;
 }
 
-
-/// \brief		Function to check if button is pressed
-/// \brief		This function checks if the mouse is in the bounding box of the button when it's clicked
-///				It changes the colour of the button as a form of click-feedback
-/// \return		This function returns TRUE if the button was in the bounding box, FALSE if it wasn't
 bool Button::is_pressed() {
 	if (boundary.getGlobalBounds().contains(sf::Vector2f{ float(sf::Mouse::getPosition(window).x), float(sf::Mouse::getPosition(window).y) })) {
 		while (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
