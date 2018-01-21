3bfa0bbb2b1e4f4171c1e6a7a52f4e71dc107eda
Draivun <nick_goris@outlook.com>
2018-01-21 15:47:09 +0100

    Update

--- project/source/main.cpp
+++ project/source/main.cpp
@@ -27,8 +27,10 @@ int main(void) {
 	sf::Sprite sprite_tile_path;
 	tile_path.loadFromFile("textures/tile_path.png");
 	sprite_tile_path.setTexture(tile_path);
+	
 	std::string play = "Play";
 	std::string exit = "Exit";
+	
 	Button play_button(play, sf::Vector2f{ float((window.getSize().x / 2)), float((window.getSize().y / 2)) }, sf::Vector2f{ 70,50 }, window);
 	Button exit_button(exit, sf::Vector2f{ float((window.getSize().x / 2)), float((window.getSize().y / 2))+window.getSize().y*0.15f }, sf::Vector2f{ 70,50 }, window);
 
@@ -36,8 +38,8 @@ int main(void) {
 	while( window.isOpen()) {
 
 		auto path = grid.find_path(start, end);
-
 		sf::Event evnt;
+		
 		while (window.pollEvent(evnt)) {
 
 			switch(evnt.type)
@@ -53,7 +55,6 @@ int main(void) {
 				}
 				if (exit_button.is_pressed()) {
 					window.close();
-					break;
 				}
 				break;
 
@@ -67,8 +68,8 @@ int main(void) {
 				//continue game
 				break;
 			case sf::Event::MouseButtonPressed:
-				if (event.mouseButton.button == sf::Mouse::Left) {
-					grid.clicked(event.mouseButton.x, event.mouseButton.y);
+				if (evnt.mouseButton.button == sf::Mouse::Left) {
+					grid.clicked(evnt.mouseButton.x, evnt.mouseButton.y);
 				}
 
 				//if (event.mouseButton.button == sf::Mouse::Right) {
@@ -96,9 +97,8 @@ int main(void) {
 
 			}
 		}
-		game.update();
-		//std::this_thread::sleep_for(std::chrono::seconds(1));
-		//sf::sleep(sf::milliseconds(20));
+		//game.update();
+
 		window.clear();
 		
 		grid.draw(window);
@@ -110,6 +110,7 @@ int main(void) {
 		//exit_button.draw();
 		//play_button.draw();
 		window.display();
+		game.update();
 	}
     return 0;
 }
