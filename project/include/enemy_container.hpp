da78048cbf8c07ca85cccb201f903597a8866e37
Dylan van Eck <dylan.v.eck@hotmail.com>
2018-01-24 15:30:00 +0100

    add documentation and cleaned up code

--- project/include/enemy_container.hpp
+++ project/include/enemy_container.hpp
@@ -5,35 +5,58 @@
 #include "enemy.hpp"
 #include "enemy_a.hpp"
 #include "enemy_b.hpp"
+
 /**
 * @file enemy_container.hpp
 * @author Dylan van Eck
-* @date 18/01/2018
-*
-* Template class with a STL container vector where pointers will be set.
+* @date 24/01/2018
+* 
+* @brief  Class for organizing a STL container map
+* 
+* Class with a STL container map where int index will be set at first 
+* and pointers of type enemy will be set at second. 
+* With the methods needed to remove and add pointers
 *
 */
-
 class Enemy_container {
 private:
-	//the vector container for to set pointers to
+
+	//The STD container where everything will be saved
 	std::map< int, Enemy*> enemy_container;
+	
+	//The number to indentify a Enemy pointer
 	int count = 0;
 
 public:
-
-	Enemy_container() {};
 	/**
-	* @brief Function for drawing the enemy
+	* @brief Default constructor
 	*
-	* @param[in] pointer		object 
+	* If this constructor is called, it will make the map with the count at zero.
+	*/
+	Enemy_container() {};
+
+	/**
+	* @brief Function for adding a Enemy to the container
+	* 
+	* It is for now a default Enemy_a what will be added to the container.
 	*/
 	void add();
 
+	/**
+	* @brief Function for deleting a Enemy from the container by index
+	*
+	* The pointer will be erased from the std::map, thereafter the Enemy will be destructed.
+	*
+	* @parameter	index			To locate which enemy pointer has to be removed
+	*/
 	void remove(int index);
 
+	/**
+	* @brief Function to return the map by using a for auto loop
+	*
+	* @return	std::map< int, Enemy*>	The enemy_container
+	*/
 	std::map< int, Enemy*> get_container();
 
 };
-
 #endif // ENEMY_CONTAINER_HPP
