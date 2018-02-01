576f3cd14391bc6e9ab6dd63e6cbf8f9cc8041fc
Draivun <nick_goris@outlook.com>
2018-02-01 10:50:53 +0100

    Updated doxygen for image and cutscene, added all cutscenes to release folder

--- project/include/cutscene.hpp
+++ project/include/cutscene.hpp
@@ -4,6 +4,16 @@
 #include <SFML/Graphics.hpp>
 #include "image.hpp"
 
+
+/**
+* @file cutscene.hpp
+* @author Nick Goris, with help from the Teenage Mutant Ninja Unicorns
+* @date 31/1/18
+*
+* @brief Class to play scenes (images from file)
+*
+* This class can bundle scenes to actually play them as a cutscene
+*/
 class cutscene {
 private:
     sf::RenderWindow & window;
@@ -22,8 +32,19 @@ private:
     image_from_file scene13;
     image_from_file scene14;
 public:
+
+    /**
+    * @brief cutscene constructor
+    *
+    * @param[in] window The window that a cutscene should be displayed on
+    */
     cutscene(sf::RenderWindow & window);
 
+    /**
+    * @brief This function plays a specific set of scenes
+    *
+    * This function draws and displays an image, waits for a given amount of time and then clears the screen again, to create the beginning cutscene of our game.
+    */
     void play_scene();
 };
 
